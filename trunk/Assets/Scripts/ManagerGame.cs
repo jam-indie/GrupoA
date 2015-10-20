ff4be1458590558ff6c138c1b2c0bd1ff8bcfe89
AndreFelipe1997 <Andrefelipe1997@yahoo.com>
2015-10-20 04:30:49 -0200

    Adicionando a pasta com o Project Settings;
    Efetuando modificações no editor da Unity:
    
    Transformando todos os elementos de HUD e "visuais" (Back -> Background, ScoreText, GameTimeText) em elementos de um Canvas, e os editando para se tornarem visíveis.
    
    Adicionadas duas linhas de teste no script ManagerGame.cs para mostrar que os valores da HUD estão funcinando e sendo alterados conforme comando do script ManagerGame.cs.
    
    Boa sorte ai rapazes (y)

--- trunk/Assets/Scripts/ManagerGame.cs
+++ trunk/Assets/Scripts/ManagerGame.cs
@@ -13,30 +13,27 @@ public class ManagerGame : MonoBehaviour
 
 	public static ManagerGame UIInstance;
 
-	// Use this for initialization
 	void Start () 
 	{
-
 		UIInstance = this;
-
-
+		
 		score = 0;
 		gametime = 20;
 
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
-
 		ScoreText.text = "Score:" + score ;
 		GameTimeText.text = "Time:" + gametime; 
-
+		//Essas duas linhas sao para mostrar que os valores na tela do game estao mudando
+		gametime --;
+		score ++;
 	}
 
-	void OnGui()
+	/*void OnGui()//Com a UI, nao precisamos usar a void OnGui e coisas relacionadas; 90% do que e exibido na tela pro player e feito no editor da Unity, os outros 10% sao script para mudar valores.
 	{
-//		GUI.Label (new Rect (Screen.width * 1f, Screen.height * 0.95, 50, 15), "Score: " + score);
+		//GUI.Label (new Rect (Screen.width * 1f, Screen.height * 0.95, 50, 15), "Score: " + score);
 		Debug.Log ("GUIOK");
-	}
+	}*/
 }
