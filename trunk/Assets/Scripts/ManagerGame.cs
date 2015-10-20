e6f28c2509bfb82b8070dcff7d6f5f88eee9e3e2
NTEsteves <nata.sesteves@gmail.com>
2015-10-20 02:35:50 -0200

    Primeiro commit dentro da pasta da jam

---
+++ trunk/Assets/Scripts/ManagerGame.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ManagerGame : MonoBehaviour 
+{
+	public static int score;
+	public static int timeoff; // tempo para acrescentar pontos no score
+	public static int gametime;
+
+	public Text ScoreText; 
+	public Text GameTimeText;
+
+	public static ManagerGame UIInstance;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		UIInstance = this;
+
+
+		score = 0;
+		gametime = 20;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		ScoreText.text = "Score:" + score ;
+		GameTimeText.text = "Time:" + gametime; 
+
+	}
+
+	void OnGui()
+	{
+//		GUI.Label (new Rect (Screen.width * 1f, Screen.height * 0.95, 50, 15), "Score: " + score);
+		Debug.Log ("GUIOK");
+	}
+}
---
+++ trunk/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour 
+{	
+	private Transform transf;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		transf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log(move());
+	}
+
+	private string move()
+	{ 
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transf.Translate(new Vector3(-0.2f, 0, 0));
+			return "LEFT";
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transf.Translate(new Vector3(0.2f, -0, 0));
+			return "RIGHT";
+		}
+		else
+		{
+			transf.Translate(new Vector3(0f, 0.001f, 0));
+			return "UP";
+			//return "IDLE";
+		}
+	
+	}
+}	
+	
